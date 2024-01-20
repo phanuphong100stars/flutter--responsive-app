24a78cc657709e13083238fc684a9e0ffb929280
phanuphong100stars <panuphong@100stars.com>
2024-01-20 21:39:19 +0700

    feat: add google_fonts and http dependencies, update the seedColor in ThemeData

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Responsive Flutter Demo',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.black),
         useMaterial3: true,
       ),
       home: const MyHomePage(title: 'Responsive Flutter Demo '),
