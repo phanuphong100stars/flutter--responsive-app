73db78b0ff19df4a7578362c22834d29bfdb2e8d
phanuphong100stars <panuphong@100stars.com>
2024-01-20 17:28:00 +0700

    refactor: replace CircularProgressIndicator with const CircularProgressIndicator

--- lib/main.dart
+++ lib/main.dart
@@ -70,7 +70,7 @@ class _MyHomePageState extends State<MyHomePage> {
               future: testService.test(),
               builder: (context, snapshot) {
                 if (snapshot.connectionState == ConnectionState.waiting) {
-                  return CircularProgressIndicator();
+                  return const CircularProgressIndicator();
                 } else if (snapshot.hasError) {
                   return Text('Error: ${snapshot.error}');
                 } else {
